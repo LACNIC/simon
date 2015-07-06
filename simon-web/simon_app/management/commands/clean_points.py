23592725f45a53b71c782b1edf806d71b881c8a4
aguformoso <aguformoso@gmail.com>
2015-07-06 16:20:52 -0300

    Minor changes

--- simon-web/simon_app/management/commands/clean_points.py
+++ simon-web/simon_app/management/commands/clean_points.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from simon_app.models import SpeedtestTestPoint
 from urllib2 import urlopen, HTTPError
+from sys import stdout
 
 __author__ = 'agustin'
 
@@ -9,19 +10,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         tps = SpeedtestTestPoint.objects.filter(enabled=True)
         N = len(tps)
-        i=0
-        for t in tps:
-            i+=1
+        disabled = []
+        for i, t in enumerate(tps):
+            stdout.write("\r%.2f%%" % (100.0 * i / N))
+            stdout.flush()
+
             try:
                 urlopen("http://" + t.ip_address + "/", timeout=5).read()
             except HTTPError as e:
                 httpCode = e.code
                 if httpCode != 200:
                     t.enabled = False
-                    print t
                     t.save()
-                    print t
+                    disabled.append(t)
                 continue
             except Exception:
                 continue
-            print "%.2f%%" % (100.0*i / N)
+        print ""
+        print "The following points have been disabled ", disabled
