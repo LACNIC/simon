0c0170fd343a082f6fe7100fb084096704485977
aguformoso <aguformoso@gmail.com>
2015-09-03 12:07:57 -0300

    Fixed the python parallel script

--- simon-web/normalization-tests/parallel.py
+++ simon-web/normalization-tests/parallel.py
@@ -7,12 +7,14 @@ json_name = sys.argv[2]
 
 with open(json_name, "r") as f:
     obj = json.loads(f.read())
-
 num_of_tests = len(obj)
+
 process = []
+
 for counter in range(num_of_tests):
-    cmd = "python " + str(file_name) + " " + str(json_name) + " " + str(counter)
+    cmd = "python %s %s %s" % (str(file_name), str(json_name), str(counter))
     process.append(subprocess.Popen(cmd, shell=True))
 
+# Wait for all chunk's processes to finish (n subprocesses)
 for counter in range(num_of_tests):
     process[counter].wait()
