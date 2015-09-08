88970f78398470fd146eaa4d5e77d0d63564ddfd
aguformoso <aguformoso@gmail.com>
2015-09-08 19:00:52 +0700

    Pagination for CCs No python Pagination at all

--- simon-web/simon_app/views.py
+++ simon-web/simon_app/views.py
@@ -1159,16 +1159,16 @@ def atlas(request):
 
     counter = OrderedDict(sorted(counter.items(), key=lambda t: t[0]))
 
-    paginator = Paginator(probes_all, 15)
-    page = request.GET.get('page')
-    try:
-        probes_paginated = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        probes_paginated = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        probes_paginated = paginator.page(paginator.num_pages)
+    # paginator = Paginator(probes_all, 15)
+    # page = request.GET.get('page')
+    # try:
+    #     probes_paginated = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # If page is not an integer, deliver first page.
+    #     probes_paginated = paginator.page(1)
+    # except EmptyPage:
+    #     # If page is out of range (e.g. 9999), deliver last page of results.
+    #     probes_paginated = paginator.page(paginator.num_pages)
 
     ctx = {
         'probes': probes_all,
