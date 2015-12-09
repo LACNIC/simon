3490a7de6fd3c2cbdfc17127bf7d62dc235aa7c8
Agustín <aguformoso@gmail.com>
2015-12-09 15:14:46 -0300

    Cleaning some Vagrant files Added requirements.txt file

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Installing dependencies"
+
 apt-get update
 
 apt-get -y --force-yes install \
@@ -14,12 +16,17 @@ sudo apt-get install postgresql \
 postgresql-contrib \
 postgresql-client-common
 
-# wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm get-pip.py && echo "pip instalado"
+pip install -r requirements.txt
 
-pip install django==1.7
+pip install django==1.6
 pip install lxml
 pip install netaddr
 pip install virtualenv
 pip install geoip2
+pip install django-admin-bootstrapped==2.3.5
+pip install django-cors-headers==1.0.0
+pip install trparse==0.1.0
+pip install newrelic==2.50.0.39
 
 echo "Finalizó la instalación de las dependencias de la VM..."
