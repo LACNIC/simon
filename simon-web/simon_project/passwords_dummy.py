fc1c2d716debb7e3a931c050aef4db559096c404
Agustín <aguformoso@gmail.com>
2015-12-24 18:16:30 -0300

    Vamosss 🎅🏻

---
+++ simon-web/simon_project/passwords_dummy.py
@@ -0,0 +1,30 @@
+EMAIL_USE_TLS = True
+
+# EMAIL_HOST = 'mail.lacnic.net.uy'
+# EMAIL_PORT = 993
+# EMAIL_HOST_USER = 'authusr'
+# EMAIL_HOST_PASSWORD = 'rew4ud3wum'
+# SERVER_EMAIL = 'agustin@lacnic.net'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aguformoso@gmail.com'
+EMAIL_HOST_PASSWORD = 'L4g3rb33r'
+SERVER_EMAIL = 'aguformoso@gmail.com'
+
+DEFAULT_FROM_EMAIL = "agustin@lacnic.net"
+
+DBNAME = 'simon'
+DBUSER = 'postgres'
+DBPASSWORD = 'postgres'
+DBHOST = '127.0.0.1'
+DBPORT = '5432'
+
+PROBEAPI = "9wdWKabvL5mshvXnH2atqtxee5ddp1eTB29jsnSJNZpeUhn3nT"
+
+TWITTER = {
+           'consumer_key' : 'Q5oMK58gtXMuNeqXFw3lAaktx',
+           'consumer_secret' : 'KRPrrWGml2h47VZK57DtEBTnjRvxtGyTuTebyEo5P0jx68w0C9',
+           'access_token' : '2704001228-QghMZ6yrXXqTIw77gXewDpatgHqNjrakRTtzI5z',
+           'access_token_secret' : '4ppX9Bdq65BEihIEOrdtf3RhKjhISCBr039ogtEjIfDfp'
+           }
