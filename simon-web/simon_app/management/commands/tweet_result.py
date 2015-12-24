703e05a9a1ec7f0a97234c6d6bb0effd0303e671
Agustín <aguformoso@gmail.com>
2015-12-24 16:56:53 -0300

    Small tweets refactoring

--- simon-web/simon_app/management/commands/tweet_result.py
+++ simon-web/simon_app/management/commands/tweet_result.py
@@ -1,6 +1,7 @@
 __author__ = 'agustin'
+import random
 from simon_app.management.commands.tweet import *
-from simon_app.models import Results
+from simon_app.models import *
 from django.core.management.base import BaseCommand
 
 
@@ -8,14 +9,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         import random
 
-        rs = Results.objects.get_daily_results().exclude(country_origin="", country_destination="")
+        rs = Results.objects.get_daily_results()
 
         if len(rs) == 0:
             exit(1)
 
-        r = rs[random.randint(0, len(rs) - 1)]
+        r = random.choice(rs)
         # 2-sigma is roughly equal to 95% confidence interval
-        text = "Daily result %s --> %s %s ms (+-%s ms)" % (
-        r.country_origin, r.country_destination, r.ave_rtt, 2 * r.dev_rtt)
+        text = "Resultado de hoy %s --> %s %s ms (+-%s ms, via %s protocol)" % (r.country_origin, r.country_destination, r.ave_rtt, 2 * r.dev_rtt, r.protocol())
 
         tweet(text)
