61f8f208e36081cd2e4ba54a64b896939abc0f93
Agustín <aguformoso@gmail.com>
2016-05-02 11:42:22 -0300

    😁

--- simon-web/simon_project/passwords_dummy.py
+++ simon-web/simon_project/passwords_dummy.py
@@ -8,11 +8,11 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aguformoso@gmail.com'
-EMAIL_HOST_PASSWORD = 'L4g3rb33r'
-SERVER_EMAIL = 'aguformoso@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+SERVER_EMAIL = '@gmail.com'
 
-DEFAULT_FROM_EMAIL = "agustin@lacnic.net"
+DEFAULT_FROM_EMAIL = "@lacnic.net"
 
 DBNAME = 'simon'
 DBUSER = 'postgres'
@@ -20,11 +20,11 @@ DBPASSWORD = 'postgres'
 DBHOST = '127.0.0.1'
 DBPORT = '5432'
 
-PROBEAPI = "9wdWKabvL5mshvXnH2atqtxee5ddp1eTB29jsnSJNZpeUhn3nT"
+PROBEAPI = ""
 
 TWITTER = {
-           'consumer_key' : 'Q5oMK58gtXMuNeqXFw3lAaktx',
-           'consumer_secret' : 'KRPrrWGml2h47VZK57DtEBTnjRvxtGyTuTebyEo5P0jx68w0C9',
-           'access_token' : '2704001228-QghMZ6yrXXqTIw77gXewDpatgHqNjrakRTtzI5z',
-           'access_token_secret' : '4ppX9Bdq65BEihIEOrdtf3RhKjhISCBr039ogtEjIfDfp'
+           'consumer_key' : '',
+           'consumer_secret' : '',
+           'access_token' : '',
+           'access_token_secret' : ''
            }
