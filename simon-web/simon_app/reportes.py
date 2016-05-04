a1e7153e30538ae61c0df9d3b614a59712b05603
Agustín <aguformoso@gmail.com>
2016-05-04 16:49:31 -0300

    Rebasing from as_reports

--- simon-web/simon_app/reportes.py
+++ simon-web/simon_app/reportes.py
@@ -6,10 +6,91 @@
 
 from __future__ import division
 from django import forms
+<<<<<<< 34970630a57f7380993ae1a5a5b589b06efa2c99
 from simon_app.models import Country, Images
 from datetime import tzinfo, timedelta
 from django.db.models import Q
 import datetime
+=======
+from simon_app.models import Country, Results, ThroughputResults, Images, AS
+from django.forms.fields import ChoiceField
+from datetime import tzinfo, timedelta
+from django.db.models import Q
+import datetime
+import re
+
+
+# class ResultsForm(forms.Form):
+# countries = Country.objects.get_region_countries().values('iso', 'printable_name').order_by('printable_name')
+# countries_list = []
+#
+#     for country in countries:
+#         register = []
+#         register.append(country['iso'])
+#         register.append(country['printable_name'])
+#         countries_list.append(register)
+#     countries = ChoiceField(countries_list, label='País')
+#
+#     testers_list = []
+#     testers_rs = Results.objects.all().distinct().values_list('tester', 'tester_version')
+#     for tester in testers_rs:
+#         register = []
+#         tester_name = str(tester[0])
+#         tester_version = re.sub('\s', '', str(tester[1]))
+#
+#         if tester_name != "" and tester_version != "":
+#             tester_print_name = tester_name + " v" + tester_version
+#             tester_value = str(tester_name + "/" + tester_version)
+#             register.append(tester_value)
+#             register.append(tester_print_name)
+#             testers_list.append(register)
+#     tester = ChoiceField(testers_list, label='Medidor')
+#
+#     #test_type = forms.ModelChoiceField(queryset=Results.objects.all().values_list('testype', flat=True).distinct(), label='Test type', initial='')
+#     ip_version = forms.ModelChoiceField(queryset=Results.objects.all().values_list('ip_version', flat=True).distinct(), label='Versión de protocolo IP', initial='')
+#
+#     oldest_year = Results.objects.all().order_by('date_test').values_list('date_test', flat=True).distinct()[0].year
+#     current_year = datetime.datetime.now().year
+#     year = forms.ChoiceField(choices=((str(x), x) for x in range(int(oldest_year), int(current_year) + 1)), label='Año desde')
+#     month = forms.ChoiceField(choices=((str(x), x) for x in range(1, 12 + 1)), label='Mes desde')
+
+
+class ThroughputResultsForm(forms.Form):
+    countries = Country.objects.all().values('iso', 'printable_name').order_by('printable_name')
+    countries_list = []
+
+    for country in countries:
+        register = []
+        register.append(country['iso'])
+        register.append(country['printable_name'])
+        countries_list.append(register)
+    countries = ChoiceField(countries_list, label='Country')
+
+    testers_list = []
+    testers_rs = ThroughputResults.objects.all().distinct().values_list('tester', 'tester_version')
+    for tester in testers_rs:
+        register = []
+        tester_name = str(tester[0])
+        tester_version = re.sub('\s', '', str(tester[1]))
+
+        if tester_name != "" and tester_version != "":
+            tester_print_name = tester_name + " v" + tester_version
+            tester_value = str(tester_name + "/" + tester_version)
+            register.append(tester_value)
+            register.append(tester_print_name)
+            testers_list.append(register)
+    tester = ChoiceField(testers_list, label='Tester')
+
+    #test_type = forms.ModelChoiceField(queryset=ThroughputResults.objects.all().values_list('testype', flat=True).distinct(), label='Test type', initial='')
+    ip_version = forms.ModelChoiceField(queryset=ThroughputResults.objects.all().values_list('ip_version', flat=True).distinct(), label='IP version', initial='')
+
+    oldest_year = ThroughputResults.objects.all().order_by('date_test').values_list('date_test', flat=True).distinct()  # [0].year
+    oldest_year = 2012
+    current_year = datetime.datetime.now().year
+    year = forms.ChoiceField(choices=((str(x), x) for x in range(int(oldest_year), int(current_year) + 1)), label='Year since')
+    month = forms.ChoiceField(choices=((str(x), x) for x in range(1, 12 + 1)), label='Month since')
+
+>>>>>>> Some progress on AS-level reports
 
 class MyImageModelMultipleChoiceField(forms.ModelMultipleChoiceField):
     def label_from_instance(self, obj):
@@ -46,34 +127,40 @@ class YearField(forms.ChoiceField):
     year = range(2009, datetime.datetime.now().year + 1)
 
 
-class ReportForm(forms.Form):
-    default_country = None
-
-    country1 = MyCountryModelChoiceField(queryset=Country.objects.get_region_countries().order_by('printable_name'),
+class ASForm(forms.Form):
+    ases = AS.objects.all()[:100]  # TODO cambiar para que abarque los regionales
+    as_dropdown = forms.ModelChoiceField(queryset=ases,
                                          widget=forms.Select(attrs={'class': 'form-control'}),
-                                         label="País de origen de las mediciones"
-    )
+                                         label="Sistema autónomo")
 
-    country2 = MyCountryModelChoiceField(queryset=Country.objects.get_region_countries().order_by('printable_name'),
-                                         widget=forms.Select(attrs={'class': 'form-control'}),
-                                         label="País de destino de las mediciones",
-                                         required=False
-    )
+    class ReportForm(forms.Form):
+        default_country = None
 
-    bidirectional = forms.NullBooleanField(
-        required=False,
-        initial=True,
-        label="Bidireccional",
-        help_text="Desmarcar esta opción si se desea filtrar mediciones en el sentido país origen --> país destino únicamente"
-    )
+        country1 = MyCountryModelChoiceField(queryset=Country.objects.get_region_countries().order_by('printable_name'),
+                                             widget=forms.Select(attrs={'class': 'form-control'}),
+                                             label="País de origen de las mediciones"
+        )
 
-    date_from = forms.DateField(
-        widget=forms.DateInput(attrs={'class': 'form-control'}),
-        label="Fecha desde"
-    )
+        country2 = MyCountryModelChoiceField(queryset=Country.objects.get_region_countries().order_by('printable_name'),
+                                             widget=forms.Select(attrs={'class': 'form-control'}),
+                                             label="País de destino de las mediciones",
+                                             required=False
+        )
 
-    date_to = forms.DateField(
-        widget=forms.DateInput(attrs={'class': 'form-control'}),
-        label="Fecha hasta",
-        required=False
-    )
+        bidirectional = forms.NullBooleanField(
+            required=False,
+            initial=True,
+            label="Bidireccional",
+            help_text="Desmarcar esta opción si se desea filtrar mediciones en el sentido país origen --> país destino únicamente"
+        )
+
+        date_from = forms.DateField(
+            widget=forms.DateInput(attrs={'class': 'form-control'}),
+            label="Fecha desde"
+        )
+
+        date_to = forms.DateField(
+            widget=forms.DateInput(attrs={'class': 'form-control'}),
+            label="Fecha hasta",
+            required=False
+        )
