2c1aa3371ea2937a82089485e6ed057a8e6e49cb
Elisa <elisapeirano90@gmail.com>
2016-05-19 16:32:22 -0300

    tweet text

--- simon-web/simon_app/management/commands/tweet_country.py
+++ simon-web/simon_app/management/commands/tweet_country.py
@@ -36,6 +36,6 @@ class Command(BaseCommand):
 
         text = "Top 5\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\nBottom 4\n %s %s\n %s %s\n %s %s\n %s %s" % (top[0][0], top[0][1], top[1][0], top[1][1], top[2][0], top[2][1], top[3][0], top[3][1], top[4][0], top[4][1],
                                                                                                                                 bottom[1][0], bottom[1][1], bottom[2][0], bottom[2][1], bottom[3][0], bottom[3][1], bottom[4][0], bottom[4][1])
-        print text
+        #print text
 
-        #tweet(text)
+        tweet(text)
