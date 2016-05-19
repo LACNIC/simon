3c799652756807ecbb685fde8a806060118218cc
Elisa <elisapeirano90@gmail.com>
2016-05-19 16:28:36 -0300

    code for getting top and bottom 5 latency measurements

---
+++ simon-web/simon_app/management/commands/tweet_country.py
@@ -0,0 +1,41 @@
+__author__ = 'elisa'
+from simon_app.management.commands.tweet import *
+from simon_app.models import *
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        import random
+
+        rs = Results.objects.inner('JavaScript', 12)
+        top = []
+        bottom = []
+        i = 0
+        order = sorted(rs, key=lambda tup: tup[1])
+        #print len(order)
+        for r in order:
+            i=i+1
+            if(i<6):
+                if(int(r[1])%10 >= 5):
+                    #print r[0], '|'*(int(r[1])/10),'+'
+                    top.append((r[0], '|'*(int(r[1])/10)+'+'))
+                else:
+                    #print r[0], '|'*(int(r[1])/10)
+                    top.append((r[0], '|'*(int(r[1])/10)))
+            elif(i>17):
+                if(int(r[1])%10 >= 5):
+                    #print r[0], '|'*(int(r[1])/10),'+'
+                    bottom.append((r[0], '|'*(int(r[1])/10)+'+'))
+                else:
+                    #print r[0], '|'*(int(r[1])/10)
+                    bottom.append((r[0], '|'*(int(r[1])/10)))
+            #suma = suma + int(r[1])/10
+
+        #print top[0][1]
+
+        text = "Top 5\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\nBottom 4\n %s %s\n %s %s\n %s %s\n %s %s" % (top[0][0], top[0][1], top[1][0], top[1][1], top[2][0], top[2][1], top[3][0], top[3][1], top[4][0], top[4][1],
+                                                                                                                                bottom[1][0], bottom[1][1], bottom[2][0], bottom[2][1], bottom[3][0], bottom[3][1], bottom[4][0], bottom[4][1])
+        print text
+
+        #tweet(text)
