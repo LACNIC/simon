1e29ac54b0c6e22db517de6a2b0d31b5a0d7e8f6
Agustín <aguformoso@gmail.com>
2016-05-27 12:57:15 -0300

    Ping command ran in parallel!

--- simon-web/simon_app/management/commands/probeapi.py
+++ simon-web/simon_app/management/commands/probeapi.py
@@ -8,33 +8,133 @@ from django.template import Template, Context
 from simon_project import passwords
 import urllib2
 import json
-from datetime import datetime
+import datetime
 import numpy
-from random import choice
+from probeapi_traceroute import get_countries, get_response
+from threading import Thread
+from Queue import Queue
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         command = "ProbeAPI Measurements"
 
-        try:
-            now = datetime.now(GMTUY())
+        def do_work(tp, url_probeapi):
+            try:
+                dt = datetime.datetime.now() - then
+                q.put((tp, url_probeapi))
+                # print "%s\t%s" % (q.unfinished_tasks, dt)
 
-            ccs = Country.objects.get_region_countries().values_list('iso', flat=True)
+                response = get_response(url_probeapi)
+                if response is not None:
+                    process_response(response, tp)
 
-            origins = ""
-            for c in ccs:
-                origins += c + ','
-            origins = origins[0:-1]  # remove trailing comma
+            except Exception as e:
+                print e
+                pass
 
-            tps = SpeedtestTestPoint.objects.distinct('country').order_by(
-                'country')  # one TP per country TODO get *really* random tests...
-            choice(tps)
-            for tp in tps:
+            finally:
+                dt = datetime.datetime.now() - then
+                q.task_done()
+                # print "%s\t%s" % (q.unfinished_tasks, dt)
+                return
+
+        def process_response(response, tp):
+            py_object = json.loads(response, parse_int=int)
+
+            if len(py_object['StartPingTestByCountryResult']) <= 0:
+                return
+
+            for result in py_object['StartPingTestByCountryResult']:
+
+                # print json.dumps(result, indent=4, separators=(',', ': '))
+
+                cc_origin = result['Country']['CountryCode']
+                asn = result['ASN']['AsnID'][2:]  # strip 'AS'
+
+                packet_loss = 0
+
+                for ping_ in result['Ping']:
+                    rtts = []
+                    for r in ping_['PingTimeArray']:
+                        try:
+                            rtts.append(int(r))
+                        except Exception as e:
+                            packet_loss += 1
+                            continue
+
+                    try:
+                        empty_ass = AS.objects.filter(network__isnull=True,
+                                                      asn=asn)  # get the asn with no network associated
+                        if len(empty_ass) <= 0:
+                            as_origin = AS(asn=asn)  # create the empty-network AS
+                            as_origin.save()
+                        elif len(empty_ass) > 1:
+                            continue
+                        else:
+                            as_origin = empty_ass[0]
+
+                    except Exception as e:
+                        as_origin = AS(asn=asn)  # create the empty-network AS
+                        as_origin.save()
+
+                    if len(rtts) <= 0:
+                        continue
+
+                    # IQR filtering...
+                    _n = len(rtts)
+                    rtts = sorted(rtts)
+                    index = len(rtts) - 1
+                    q1 = rtts[int(0.25 * index)]
+                    q3 = rtts[int(0.75 * index)]
+                    iqr = q3 - q1
+                    max = q3 + 1.5 * iqr
+                    min = q1 - 1.5 * iqr
+                    rtts = [r for r in rtts if r > min and r < max]
+
+                    if len(rtts) <= 0:
+                        continue
+
+                    as_destination = AS.objects.get_as_by_ip(destination_ip)
+                    cc_destination = tp.country  # country['CountryCode']
+
+                    std_dev = numpy.std(rtts)
+                    ProbeApiPingResult(
+                        date_test=datetime.datetime.now(),
+                        ip_origin='',
+                        ip_destination=tp.ip_address,
+                        min_rtt=numpy.amin(rtts),
+                        max_rtt=numpy.amax(rtts),
+                        ave_rtt=numpy.mean(rtts),
+                        dev_rtt=std_dev,
+                        median_rtt=numpy.median(rtts),
+                        packet_loss=packet_loss,
+                        country_origin=cc_origin,
+                        country_destination=tp.country,
+                        ip_version=6 if ':' in tp.ip_address else 4,
+                        as_origin=as_origin.asn,
+                        as_destination=as_destination.asn,
+                        url=tp.url,
+                        number_probes=len(rtts)
+                    ).save()
+
+                    print "ICMP ping from %s to %s is %.0f ms (%s samples, +- %.0f ms, %.0f samples stripped)" % (
+                        cc_origin, cc_destination, numpy.mean(rtts), len(rtts), 2 * std_dev, _n - len(rtts))
+
+        ccs = get_countries().keys()
+
+        tps = SpeedtestTestPoint.objects.get_ipv4().filter(enabled=True).distinct('country').order_by(
+            'country')
+
+        q = Queue(250)
+
+        then = datetime.datetime.now()
+
+        for tp in tps:
+
+            for cc in ccs:
 
                 destination_ip = tp.ip_address
-                cc_destination = tp.country  # country['CountryCode']
-                as_destination = AS.objects.get_as_by_ip(destination_ip)
                 count = 15
 
                 opener = urllib2.build_opener()
@@ -51,95 +151,25 @@ class Command(BaseCommand):
                              "timeout={{ timeout }}")
 
                 ctx = Context(
-                    {'cc': origins, 'count': count, 'destination': destination_ip, 'probeslimit': 10 * len(ccs),
-                     'timeout': 30000})
-                url = t.render(ctx)
+                    {
+                        'cc': cc,
+                        'count': count,
+                        'destination': destination_ip,
+                        'probeslimit': 10 * len(ccs),
+                        'timeout': 30000
+                    }
+                )
+                probeapi_url = t.render(ctx)
 
-                response = opener.open(url).read()
-                py_object = json.loads(response, parse_int=int)
+                t = Thread(
+                    target=do_work,
+                    args=(tp, probeapi_url)
+                )
+                t.daemon = False
 
-                if len(py_object['StartPingTestByCountryResult']) <= 0:
-                    continue
+                if q.full():
+                    q.join()
+                else:
+                    t.start()
 
-                for result in py_object['StartPingTestByCountryResult']:
-
-                    # print json.dumps(result, indent=4, separators=(',', ': '))
-
-                    cc_origin = result['Country']['CountryCode']
-                    asn = result['ASN']['AsnID'][2:]  # strip 'AS'
-
-                    packet_loss = 0
-
-                    for ping_ in result['Ping']:
-
-                        # for each testpoint
-                        tp = SpeedtestTestPoint.objects.get(ip_address=ping_['IP'])
-
-                        rtts = []
-                        for r in ping_['PingTimeArray']:
-                            try:
-                                rtts.append(int(r))
-                            except:
-                                packet_loss += 1
-                                continue
-
-                        try:
-                            empty_ass = AS.objects.filter(network__isnull=True,
-                                                          asn=asn)  # get the asn with no network associated
-                            if len(empty_ass) <= 0:
-                                as_origin = AS(asn=asn)  # create the empty-network AS
-                                as_origin.save()
-                            elif len(empty_ass) > 1:
-                                continue
-                            else:
-                                as_origin = empty_ass[0]
-
-                        except:
-                            as_origin = AS(asn=asn)  # create the empty-network AS
-                            as_origin.save()
-
-                        if len(rtts) <= 0:
-                            continue
-
-                        # IQR filtering...
-                        _n = len(rtts)
-                        rtts = sorted(rtts)
-                        index = len(rtts) - 1
-                        q1 = rtts[int(0.25 * index)]
-                        q3 = rtts[int(0.75 * index)]
-                        iqr = q3 - q1
-                        max = q3 + 1.5 * iqr
-                        min = q1 - 1.5 * iqr
-                        rtts = [r for r in rtts if r > min and r < max]
-
-                        if len(rtts) <= 0:
-                            continue
-
-                        std_dev = numpy.std(rtts)
-                        ProbeApiPingResult(
-                            date_test=now,
-                            ip_origin='',
-                            ip_destination=destination_ip,
-                            min_rtt=numpy.amin(rtts),
-                            max_rtt=numpy.amax(rtts),
-                            ave_rtt=numpy.mean(rtts),
-                            dev_rtt=std_dev,
-                            median_rtt=numpy.median(rtts),
-                            packet_loss=packet_loss,
-                            country_origin=cc_origin,
-                            country_destination=cc_destination,
-                            ip_version=6 if ':' in destination_ip else 4,
-                            as_origin=as_origin.asn,
-                            as_destination=as_destination.asn,
-                            url=tp.speedtest_url,
-                            number_probes=len(rtts)
-                        ).save()
-
-                        print "ICMP ping from %s to %s is %.0f ms (%s samples, +- %.0f ms, %.0f samples stripped)" % (
-                            cc_origin, cc_destination, numpy.mean(rtts), len(rtts), 2 * std_dev, _n - len(rtts))
-            status = True
-        except:
-            status = False
-        finally:
-            ca = CommandAudit(command=command, status=status)
-            ca.save()
+        q.join()
