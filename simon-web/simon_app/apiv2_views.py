0e91b9665045d9f663ca9fb0329b0a7866883f35
Elisa <elisapeirano90@gmail.com>
2016-06-20 11:40:11 -0300

    All filters for query params done

--- simon-web/simon_app/apiv2_views.py
+++ simon-web/simon_app/apiv2_views.py
@@ -139,36 +139,97 @@ urlpatterns = patterns('',
 
 def latency(request):
 
-    country='all'
-    ip_ver='all'
-    year=2009
-    month=01
-    n=50
+    # country='all'
+    # ip_ver='all'
+    # year=2009
+    # month=01
 
     year_from = request.GET.get("year_from")
     year_to = request.GET.get("year_to")
     month_from = request.GET.get("month_from")
-    month_to =request.GET.get("month_to")
+    month_to = request.GET.get("month_to")
     day_from = request.GET.get("day_from")
     day_to =request.GET.get("day_to")
     country_origin = request.GET.get("country_origin")
     country_destination = request.GET.get("country_dest")
     ip_version = request.GET.get("ip_version")
-    tester = request.GET.get("protocol")
+    protocol = request.GET.get("protocol")
     as_origin =  request.GET.get("as_origin")
     as_destination = request.GET.get("as_dest")
-    url = request.GET.get("url")
+    n = request.GET.get("n")
+    # url = request.GET.get("url")
 
-    # print request.GET.get("country")
+    # Date filters
+    if year_from == None:
+        year_from = 2009
+    if month_from == None:
+        month_from = 01
+    if day_from == None:
+        day_from = 1
+    if year_to == None:
+        year_to = datetime.date.today().year
+    if month_to == None:
+        month_to = datetime.date.today().month
+    if day_to == None:
+        day_to = datetime.date.today().day
+    date_from = datetime.date(int(year_from), int(month_from), int(day_from))
+    date_to = datetime.date(int(year_to), int(month_to), int(day_to))
+    date_filter = Q(date_test__gte = date_from) & Q(date_test__lte = date_to)
 
-    date_from = datetime.date(int(year), int(month), 1)
-    results = Results.objects.filter(Q(date_test__gt=date_from))
+    #Verifying country codes
+    cc = Country.objects.get_region_countrycodes()
 
-    if ip_ver is not 'all':
-        results = results.filter(Q(ip_version=ip_ver))
+    if str(country_origin) in cc and str(country_destination) in cc:
+        country_filter = Q(country_origin = country_origin) & Q(country_destination = country_destination)
+    elif str(country_origin) not in cc and str(country_destination) in cc:
+        country_filter = Q(country_destination = country_destination)
+    elif str(country_destination) not in cc and str(country_origin) in cc:
+        country_filter = Q(country_origin=country_origin)
+    else:
+        country_filter = Q()
 
-    if country is not 'all':
-        results = results.filter(Q(country_origin=country) | Q(country_destination=country))
+    # ASN filters
+    if str(as_origin) in settings.asns and str(as_destination) in settings.asns:
+        as_filter = Q(as_origin = as_origin) & Q(as_destination = as_destination)
+    elif str(as_origin) not in settings.asns and str(as_destination) in settings.asns:
+        as_filter = Q(as_destination = as_destination)
+    elif str(as_destination) not in settings.asns and str(as_origin) in settings.asns:
+        as_filter = Q(as_origin=as_origin)
+    else:
+        as_filter = Q()
+
+    # Verifying ip version
+    if ip_version == None or int(ip_version) != 4 and int(ip_version) !=6:
+        ip_version_filter = Q()
+    else:
+        ip_version_filter = Q(ip_version = ip_version)
+
+    # Setting tester
+    prot = ['HTTP', 'ICMP', 'NTP']
+    if protocol != None and str(protocol) in prot:
+        tester = settings.PROTOCOLS[str(protocol)]
+        tester_filter = Q(tester = tester)
+    else:
+        tester_filter = Q()
+
+    results = Results.objects.filter(date_filter, country_filter, as_filter, ip_version_filter, tester_filter)
+
+    # number of items per page
+    if n == None:
+        n = 50
+
+    # date_from = datetime.date(int(year), int(month), 1)
+    # results = Results.objects.filter(Q(date_test__gt=date_from))
+    #
+    # ip_filter = Q()
+    # if ip_ver is not 'all':
+    #     ip_filter = Q(ip_version=ip_ver)
+    #
+    # country_filter = Q()
+    # if country is not 'all':
+    #     country_filter = Q(country_origin=country) | Q(country_destination=country)
+    #
+    # results = results.filter(ip_filter, country_filter) # All filters are applied
 
     paginator = Paginator(results, n)
 
