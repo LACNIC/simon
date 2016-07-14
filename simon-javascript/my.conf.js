1e50903c69ae059596a4cce15c4acc718984e13d
Agustín <aguformoso@gmail.com>
2016-07-14 14:51:59 +0200

    no message

--- simon-javascript/my.conf.js
+++ simon-javascript/my.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],//, 'Safari', 'Opera', 'IE'],
+    browsers: ['Chrome', 'Firefox', 'Safari'],//, 'Opera', 'IE'],
 
 
     // Continuous Integration mode
