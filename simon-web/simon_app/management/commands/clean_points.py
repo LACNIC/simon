7d041e425dc9e29ceaf3096416d88df62472b739
Agustín <aguformoso@gmail.com>
2016-07-24 21:19:10 +0200

    Second hotfix...

--- simon-web/simon_app/management/commands/clean_points.py
+++ simon-web/simon_app/management/commands/clean_points.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from simon_app.models import SpeedtestTestPoint
-from sys import stdout
 from simon_app.decorators import chatty_command
 
 __author__ = 'agustin'
@@ -14,9 +13,10 @@ class Command(BaseCommand):
         N = len(tps)
         disabled = []
         for i, tp in enumerate(tps):
-            stdout.write("\r%.2f%%" % (100.0 * i / N))
-            stdout.flush()
-
-            tp.check_point()
+            try:
+                tp.check_point()
+            except Exception as e:
+                print e
+                continue
 
         return "The following points have been disabled %s" % (disabled)
