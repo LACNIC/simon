0904ff7524281484fad1e7c13404187fccbde59c
Agustín <aguformoso@gmail.com>
2016-08-14 22:13:27 +0200

    Better styles and Feedback form

--- simon-web/simon_app/forms.py
+++ simon-web/simon_app/forms.py
@@ -1,6 +1,23 @@
 __author__ = 'agustin'
 from django import forms
 
+
 class FeedbackForm(forms.Form):
-    mensaje = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'style':'background-color:#F1EFEE'}))
-    remitente = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control', 'style':'background-color:#F1EFEE'}))
+    mensaje = forms.CharField(
+        help_text="",
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'style': 'margin-bottom: .5em',
+                'placeholder': "Tu mensaje..."
+            }
+        ))
+    remitente = forms.EmailField(
+        required=False,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'style': 'margin-bottom: .5em',
+                'placeholder': "Remitente"
+            }
+        ))
