ceceffb7cf984e2607d17bcf669990065c3a7792
Agustín <aguformoso@gmail.com>
2016-08-15 14:44:02 +0200

    no message

--- simon-web/simon_app/management/commands/probeapi_afrinic.py
+++ simon-web/simon_app/management/commands/probeapi_afrinic.py
@@ -10,10 +10,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         msm = ProbeApiMeasurement()
         afrinic_countrycodes = Country.objects.get_afrinic_countrycodes()
-        msm.init(tps=SpeedtestTestPoint.objects.get_ipv4().
-                 filter(enabled=True).
-                 distinct('country').
-                 order_by('country').
-                 filter(country__in=afrinic_countrycodes),
+        msm.init(
+            tps=SpeedtestTestPoint.objects.get_ipv4().
+                filter(enabled=True).
+                distinct('country').
+                order_by('country').
+                filter(country__in=afrinic_countrycodes),
 
-                 ccs=afrinic_countrycodes)
+            ccs=afrinic_countrycodes)
