24c105a5de0ed774198dbcdd482f0ba5ae5d0f73
Agustín <aguformoso@gmail.com>
2016-10-07 15:13:13 +0200

    Fixed billing column

--- simon-web/simon_app/admin.py
+++ simon-web/simon_app/admin.py
@@ -178,7 +178,7 @@ class ProbeApiAuditAdmin(CommandAuditAdmin):
     month.short_description = "Month count"
 
     def billed(self, obj):
-        return ".0f EUR" % float(self.month(obj)*self.rate)
+        return "%.0f EUR" % float(self.month(obj)*self.rate)
 
     billed.short_description = "Expected invoice"
 
