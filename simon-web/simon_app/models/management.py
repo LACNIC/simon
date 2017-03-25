943099d113afa0ebde964e1f5f26cc9481d5f691
Agustín <aguformoso@gmail.com>
2017-03-25 15:44:11 -0500

    tz aware datetime for management

--- simon-web/simon_app/models/management.py
+++ simon-web/simon_app/models/management.py
@@ -1,16 +1,22 @@
 from django.db import models
 from datetime import datetime
+import pytz
 
 __author__ = 'agustin'
+
+
+def now():
+    return datetime.now(tz=pytz.timezone('America/Montevideo'))
 
 
 class CommandAudit(models.Model):
     """
         Class in charge of general command auditing
     """
+
     command = models.CharField(max_length=100)
     description = models.TextField(max_length=10240, default="Everything OK")
-    date = models.DateTimeField(default=datetime.now())
+    date = models.DateTimeField(default=now)
     status = models.BooleanField(default=True)  # status of the command that has been just ran
 
 
