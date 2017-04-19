b71d58c40558fb1ad8390a679edb87413c6feb21
Agustín <aguformoso@gmail.com>
2017-04-19 19:11:32 -0300

    More elegant admin name

---
+++ simon-web/simon_app/apps.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SimonAppConfig(AppConfig):
+    name = 'simon_app'  # do not change
+    verbose_name = 'Proyecto Simón - Aplicación Web'
