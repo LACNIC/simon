491f0a2741578c4ff0354c672d06d5ad6ec00e74
Agustín <aguformoso@gmail.com>
2017-05-11 15:10:32 -0300

    Housekeeping

--- simon-web/simon_project/settings.py
+++ simon-web/simon_project/settings.py
@@ -326,8 +326,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # "/home/agustinf/Escritorio/simon_project/simon_app/templates"
-    # ("simon_app/templates")
 )
 
 INSTALLED_APPS = (
