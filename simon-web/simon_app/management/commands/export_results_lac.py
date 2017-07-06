d22325017c104629125b2be8abfff3bd18280f5d
Agustín <aguformoso@gmail.com>
2017-07-06 19:48:59 -0300

    no message

--- simon-web/simon_app/management/commands/export_results_lac.py
+++ simon-web/simon_app/management/commands/export_results_lac.py
@@ -8,14 +8,14 @@ from simon_app.models import Country, ProbeApiPingResult
 
 class Command(BaseCommand):
     """
-    Command to export Africa Connectivity results to the world
+    Command to export LAC Region Connectivity results to the world
     """
 
     def handle(self, *args, **options):
         comments = []
 
-        ccs = Country.objects.get_afrinic_countrycodes()
-        start = datetime(year=2017, month=03, day=21)
+        ccs = Country.objects.get_lacnic_countrycodes()
+        start = datetime(year=2016, month=07, day=01)
 
         sms = ProbeApiPingResult.objects.filter(
             Q(country_destination__in=ccs) & Q(country_origin__in=ccs)
@@ -23,18 +23,4 @@ class Command(BaseCommand):
             date_test__gte=start
         )
 
-        export(sms, 'results-africa-connectivity')  # .json
-
-        # top 28
-        top28 = ['jumia.com.ng', 'konga.com', 'bidorbuy.co.za', 'fnb.co.za', 'gtbank.com', 'absa.co.za',
-                 'standardbank.co.za', 'almasryalyoum.com', 'elkhabar.com', 'vanguardngr.com', 'news24.com',
-                 'punchng.com', 'iol.co.za', 'ghanaweb.com', 'nairaland.com', 'supersport.com', 'alwafd.org',
-                 'iroking.com']
-
-        trs_top28 = ProbeApiPingResult.objects.filter(
-            Q(country_origin__in=ccs) & Q(url__in=top28)
-        ).filter(
-            date_test__gte=start
-        )
-
-        export(trs_top28, 'results-africa-connectivity-top28')
+        export(sms, 'results-lac-connectivity')  # .json
