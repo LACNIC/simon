c5865f26ef052e6b409b6666cbb2bb4b8eb05a0a
Agustín <aguformoso@gmail.com>
2017-09-25 14:48:05 -0300

    v6perf for 2 7 and 30 days time windows

--- simon-web/simon_app/management/commands/v6perf.py
+++ simon-web/simon_app/management/commands/v6perf.py
@@ -6,22 +6,32 @@ import requests, json
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        url = 'https://stats.labs.apnic.net/v6perf'
-        # url = 'http://localhost:8000/static/simon_app/v6perf.json'
-        domstring = requests.get(url).text
 
-        diff_list = domstring.split('[\'Country\', \'Mean RTT Diff\'],')[1].split(']);')[0].replace('\'', '"')
-        s_diff_list = "[%s]" % diff_list
-        for item in json.loads(s_diff_list):
-            cc = item[0]
-            diff = item[1]
-            #         v6_rate = row[V6USERRATE]
-            #         now = datetime.datetime.now()
-            #         dualstack = row[DUALSTACK]
-            #
-            v6perf = V6Perf(
-                country=cc,
-                diff=float(diff),
-                date=datetime.now()
-            )
-            v6perf.save()
+        """
+            V6/V4 RTT comparison
+
+        :param args:
+        :param options:
+        :return:
+        """
+
+        for time_window in [2, 7, 30]:
+            url = "https://stats.labs.apnic.net/v6perf?w=%d" % time_window
+            # url = 'http://localhost:8000/static/simon_app/v6perf.json'
+            domstring = requests.get(url).text
+
+            diff_list = domstring.split('[\'Country\', \'Mean RTT Diff\'],')[1].split(']);')[0].replace('\'', '"')
+            s_diff_list = "[%s]" % diff_list
+            for item in json.loads(s_diff_list):
+                cc = item[0]
+                diff = item[1]
+                #         v6_rate = row[V6USERRATE]
+                #         now = datetime.datetime.now()
+                #         dualstack = row[DUALSTACK]
+                v6perf = V6Perf(
+                    country=cc,
+                    diff=float(diff),
+                    date=datetime.now(),
+                    time_window=time_window
+                )
+                v6perf.save()
