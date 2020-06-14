c696c1360995e80af1b70556619b311e08ee7292
Agustin Formoso <aguformoso@gmail.com>
2020-06-14 09:31:43 +0200

    simon-wimi

--- simon-wimi/cemd_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class IpRequestQuery(models.Model):
-    ip_address = models.GenericIPAddressField()
-    date_request = models.DateTimeField('request date')
-    
-    def __unicode__(self):
-        return self.ip_address
