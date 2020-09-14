252251e4fd39c15587c1dd9117851b3a896c0f88
Agustin Formoso <aguformoso@gmail.com>
2020-09-14 20:22:57 +0200

    Better and faster `clean_points` command

--- simon-web/simon_app/management/commands/clean_points.py
+++ simon-web/simon_app/management/commands/clean_points.py
@@ -1,10 +1,10 @@
+from collections import defaultdict
+from django.db import DatabaseError, transaction
 from django.core.management.base import BaseCommand
 from simon_app.models import SpeedtestTestPoint
 from simon_app.decorators import chatty_command
-import sys
-from Queue import Queue
-from threading import Thread
-from collections import defaultdict
+from multiping import MultiPing, multi_ping
+from tqdm import tqdm
 
 __author__ = 'agustin'
 
@@ -14,48 +14,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         tps = SpeedtestTestPoint.objects.all()
 
-        results = defaultdict(lambda: defaultdict(bool))
-        concurrent = 100
+        responses, no_responses = multi_ping([tp.ip_address for tp in tps], timeout=10, retry=9)
+        tps.filter(ip_address__in=responses.keys()).update(enabled=True)
+        tps.exclude(ip_address__in=responses.keys()).update(enabled=False)
 
-        def do_work():
-            while True:
-                tp = q.get()
-                before = tp.enabled
-                enabled = tp.check_point()
-                after = enabled
-
-                results[tp.url]['before'] = before
-                results[tp.url]['after'] = after
-
-                q.task_done()
-
-        q = Queue(concurrent)
-        for i in range(concurrent):
-            t = Thread(target=do_work)
-            t.daemon = True
-            t.start()
-
-        try:
-            for tp in tps[:]:
-                q.put(tp)
-            q.join()
-        except KeyboardInterrupt:
-            sys.exit(1)
-
-        up = []
-        down = []
-        still_up = []
-        for url, statuses in results.iteritems():
-            _before = statuses['before']
-            _after = statuses['after']
-            if _before != _after and not _after:
-                down.append(url)
-            elif _before != _after and _after:
-                up.append(url)
-            elif _after:
-                still_up.append(url)
-
-        if up: print "UP ", up
-        if down: print "DOWN ", down
-        if still_up: print "STILL UP ", still_up
-        return "The following points have been disabled"
+        print "Test points statuses (ICMP):"
+        print "UP {up}".format(up=len(responses)), tps.filter(ip_address__in=responses.keys())
+        print "DOWN {down}".format(down=len(no_responses)), tps.exclude(ip_address__in=responses.keys())
